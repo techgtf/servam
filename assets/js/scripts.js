5ca189a76beb78c9d06f4e78ffee5ac74bafe7d3
techgtf <bhupendra.singh@gtftechnologies.com>
2025-11-25 15:00:32 +0530

    3 more section desktop

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,29 @@
+let partnersSlider = new Swiper(".partners-slider", {
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 1500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 4,
+        },
+        1024: {
+            slidesPerView: 6,
+        },
+        1440: {
+            slidesPerView: 7,
+        },
+    },
+});
 
 
 const modal = $(".custom-modal")
